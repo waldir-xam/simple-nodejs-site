c1c49bcaaf01c9b8645362056cbf6ba379674728
waldir-xam <waliuxd@gmail.com>
2024-04-12 02:30:04 -0500

    setted up for deploy

--- src/index.js
+++ src/index.js
@@ -2,18 +2,24 @@ import express from "express";
 import { dirname, join } from "path";
 import { fileURLToPath } from "url";
 
+/* routes */
 import indexRoutes from "./routes/index.js";
 
+/* init express */
 const app = express();
-
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
+/* settings */
+app.set("port", process.env.PORT || 4000);
 app.set("views", join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+/* routes */
 app.use(indexRoutes);
 
+/* static files */
 app.use(express.static(join(__dirname, "public")));
 
-app.listen(4000);
-console.log("server is listening on port 4000");
+/* listenint to the server */
+app.listen(app.get("port"));
+console.log("Server on port", app.get("port"));
